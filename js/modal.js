fa4ea408584ca0f10a00b6ed7a99356f478a99b7
RicRocky <ryzerogtcah@gmail.com>
2025-03-09 03:37:42 +0700

    [Responsif Update] Donasi, Status Transaksi, Header, Footer

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,15 @@
+const Tampil = () => {
+    let nodeModal = document.getElementById("modal");
+    nodeModal.setAttribute("class", "c-container-modal");
+}
+
+
 const Close = () => {
     let nodeModal = document.getElementById("modal");
-    nodeModal.setAttribute("class", "c-hide");
+    nodeModal.setAttribute("class", "c-container-modal c-hide");
+    Direct();
 }
 
 const Direct = () => {
-    window.location.replace("beranda.html")
+    window.location.replace("../beranda.html")
 } 
